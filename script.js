d0959a0c9fc63fd545523ee95ef40fedd63e407c
CHINNAKRISHNA143 <chinnikrishnamekala1@gmail.com>
2026-03-17 21:02:08 +0530

    css Impl

---
+++ script.js
@@ -0,0 +1,10 @@
+const BASE_URL ="http://localhost:8080";
+
+function showSection(id){
+    document.querySelectorAll('.section').forEach(sec => sec.style.display = 'none');
+    documnet.getElementById(id).style.display='block';
+}
+
+function createAccount(){
+    
+}
