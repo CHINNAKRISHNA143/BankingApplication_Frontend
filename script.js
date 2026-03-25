77914b533ab77536360a93d963f60bd283fab72a
CHINNAKRISHNA143 <chinnikrishnamekala1@gmail.com>
2026-03-25 21:06:37 +0530

    Other APIs Impl

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const BASE_URL ="http://localhost:8080";
 
 function showSection(id){
     document.querySelectorAll('.section').forEach(sec => sec.style.display = 'none');
-    documnet.getElementById(id).style.display='block';
+    document.getElementById(id).style.display='block';
 }
 
 function createAccount(){
@@ -31,4 +31,92 @@ function deposite(){
         accNo : document.getElementById("d-acc").value,
         amount : parseFloat(document.getElementById("d-amount").value),
     };
+
+    fetch(BASE_URL+"/transactions/deposite",{
+        method:"POST",
+        headers:{
+            "Content-Type": "application/json"
+        },
+        body:JSON.stringify(data)
+    })
+    .then(res => res.text())
+    .then(msg =>{
+        document.getElementById("deposit-result").innerText = msg;
+    });
+
 }
+
+function withdraw(){
+    const data = {
+        accNo : document.getElementById("d-acc").value,
+        amount : parseFloat(document.getElementById("d-amount").value)
+    };
+
+    fetch(BASE_URL+"/tranctions/withdraw",{
+        method:"POST",
+        headers:{
+            "Content-Type": "application/json"
+        },
+        body:JSON.stringify(data)
+    })
+    .then(res => res.text())
+    .then(msg =>{
+        document.getElementById("withdraw-result").innerText = msg;
+    });
+
+}
+
+function transfer(){
+    const data = {
+        fromAcc : document.getElementById("t-from").value,
+        toAcc : document.getElementById("t-to").value,
+        amount : parseFloat(document.getElementById("t-amount").value)
+    };
+
+    fetch(BASE_URL+"/transactions/transfer",{
+        method:"POST",
+        headers:{
+            "Content-Type": "application/json"
+        },
+        body:JSON.stringify(data)
+    })
+    .then(res => res.text())
+    .then(msg =>{
+        document.getElementById("transfer-result").innerText = msg;
+    });
+
+};
+
+function viewAccount(){
+    const acc= document.getElementById("v-acc").value;
+
+    fetch(BASE_URL+"/accounts/"+acc)
+    .then(res => res.json())
+    .then(data =>{
+        document.getElementById("view-result").innerText = JSON.stringify(data,null,2);
+    });
+    
+}
+
+function listAccounts(){
+    fetch(BASE_URL+"/accounts/all")
+    .then(res => res.json())
+    .then(data => {
+        document.getElementById("list-result").innerText = JSON.stringify(data,null,2);
+
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
